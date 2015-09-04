9ba63b50dc0062d3e0c66286b91433b8d9b9bfab
JerryTseng <jerry.cc.tseng@gmail.com>
2015-09-04 15:55:30 +0800

    New Commit!!

--- HelloWorld/src/main/java/helloWorld.java
+++ HelloWorld/src/main/java/helloWorld.java
@@ -3,7 +3,7 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// TEST 123445
+		// TEST 1234459980
 		System.out.println("Hello World!!");
 	}
 
